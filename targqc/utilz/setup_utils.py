0d741758853527980eab14b22c0382b681ca7eaa
safary1094 <safary1094@yandex.ru>
2018-05-10 15:39:14 +0300

    modified:   targqc/utilz/setup_utils.py

--- targqc/utilz/setup_utils.py
+++ targqc/utilz/setup_utils.py
@@ -54,7 +54,10 @@ def clean_package(package_name, dirpath='.'):
 def get_reqs():
     
     
-    from pip._internal.req import parse_requirements
+    try: # for pip >= 10
+        from pip._internal.req import parse_requirements
+    except ImportError: # for pip <= 9.0.3
+        from pip.req import parse_requirements
     
     
     try:
